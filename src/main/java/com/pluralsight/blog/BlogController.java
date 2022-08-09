5080a99be82cfae23e5d01e89d938ef8b93beddc
Lieutenant-DJ <67519347+Lieutenant-DJ@users.noreply.github.com>
2022-08-09 15:44:20 -0400

    Module 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,26 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.pluralsight.blog.model.*;
+import java.util.List;
 
 @Controller
 public class BlogController {
+    private PostRepository postRepository;
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap){
+        List<Post> posts = postRepository.getAllPosts();
+        modelMap.put("posts", posts);
         modelMap.put("title", "Blog Post 1");
         return "home";
+    }
+
+
+    public BlogController(PostRepository postRepository){
+        this.postRepository = postRepository;
     }
 
 
